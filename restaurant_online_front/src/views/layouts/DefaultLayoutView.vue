3dd458cbfb41258ff454f54e8827b6bf94836fbe
anton <tehanovanton@gmail.com>
2022-11-23 10:47:57 +0300

    display restaurants in column

--- restaurant_online_front/src/views/layouts/DefaultLayoutView.vue
+++ restaurant_online_front/src/views/layouts/DefaultLayoutView.vue
@@ -1,22 +1,49 @@
 <script setup>
+    import axios from 'axios';
+    import { onBeforeMount, ref } from 'vue';
+    import { useTokensStore } from '../../stores/tokens'
 
+    const restaurants = ref([])
+    const tokens = useTokensStore()
+
+
+    onBeforeMount(async () => {
+        await getRestaurants()
+    })
+
+    const getRestaurants = async () => {        
+        let response = await axios.get('http://localhost:3000/restaurants', {
+            headers: tokens.auth_headers
+        })
+        .catch((error) => {            
+            console.log(error);
+        })
+        
+        if (response && response.status === 200) {
+            restaurants.value = response.data
+            tokens.setAuthTokens(response.headers)
+        }
+    }
 </script>
 
 <template>
-    <header class="block centrenize-content">
+    <header class="block centrenize-content-row">
         HEADER
     </header>
 
-    <div class="centrenize-content">
-        <div class="menu block centrenize-content">
-            MENU
+    <div class="centrenize-content-row">
+        <div class="menu centrenize-content-column">
+            <div class="block centrenize" v-for="restaurant in restaurants">
+                {{ restaurant.name }}
+            </div>
         </div>
-        <div class="content block centrenize-content">
+
+        <div class="content block centrenize-content-row">
             CONTENT
         </div>
     </div>
 
-    <footer class="block centrenize-content">
+    <footer class="block centrenize-content-row">
         FOOTER
     </footer>
 </template>
@@ -25,12 +52,25 @@
     .block {
         border: 3px solid black;
         padding: 3px;
+        margin: 3px;
     }
 
-    .centrenize-content {
+    .centrenize {
+        display: flex;        
+        justify-content: space-around;    
+    }
+
+    .centrenize-content-column {
         display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+    }
+
+    .centrenize-content-row {
+        display: flex;
+        flex-direction: row ;        
         justify-content: space-around;        
-    }    
+    }
 
     .menu {
         flex: 1;
