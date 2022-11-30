e486efac758c5dec753403025797693716948b5b
anton <tehanovanton@gmail.com>
2022-11-30 11:54:32 +0300

    index with shows that has mode switchers

---
+++ restaurant_online_front/src/views/restaurants/EditView.vue
@@ -0,0 +1,15 @@
+<script setup>
+  const props = defineProps(['restaurant'])
+</script>
+
+<template>  
+  <form class="block">
+    <p>
+      <input v-model="restaurant.name"/>
+    </p>
+
+    <button type="button">save</button>
+    <button type="button">cancel</button>
+  </form>
+
+</template>
--- restaurant_online_front/src/views/restaurants/ShowView.vue
+++ restaurant_online_front/src/views/restaurants/ShowView.vue
@@ -1,7 +1,31 @@
 <script setup>
+  import { ref } from 'vue';
+  import EditRestaurant from './EditView.vue'
+
   const props = defineProps(['restaurant'])
+  const mode = ref('show')
+
+  const setMode = (modeName) => {
+    if (mode.value !== modeName) {
+      mode.value = modeName
+    } else {
+      mode.value = modeName
+    }
+  }
 </script>
 
 <template>
-  Name: {{ restaurant.name }}
+  <label for="showModeSwitch">Show: </label>
+  <input id="showModeSwitch" name="modeSwith" type="radio" @click="setMode('show')" />
+
+  <label for="editModeSwitch">Edit: </label>
+  <input id="editModeSwitch" name="modeSwith" type="radio" @click="setMode('edit')" />
+
+  <div v-if="mode == 'show'">
+    Name: {{ restaurant.name }}
+  </div>
+
+  <div v-if="mode == 'edit'">
+    <EditRestaurant :restaurant="restaurant" />
+  </div>
 </template>
