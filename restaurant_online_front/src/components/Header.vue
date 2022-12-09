8c0cf1355caa8eeaa8b7eb4021638640e0fbd186
anton <tehanovanton@gmail.com>
2022-12-09 12:42:26 +0300

    header home button

--- restaurant_online_front/src/components/Header.vue
+++ restaurant_online_front/src/components/Header.vue
@@ -1,10 +1,15 @@
 <script setup>
+  import router from '../router/router';
   import authService from '../views/services/auth_service';
-
+  
+  const usersIndex = () => router.push({ name:'users' })
+  const homeView = () => router.push({ name:'home' })
 </script>
 
 <template>
   <header class="block centrenize-content-row">
-    <button type="button" @click="authService.apiSignOut()">sign out</button>
+    <button type="button" @click="homeView">Home</button>
+    <button type="button" @click="usersIndex">Profile</button>
+    <button type="button" @click="authService.apiSignOut()">Sign out</button>
   </header>
 </template>
