361eaf2f9ed71339747010748d3e9a2dcee43798
anton <tehanovanton@gmail.com>
2023-04-13 19:06:40 +0300

    Restaurant form style

--- restaurant_online_front/src/components/RestaurantForm.vue
+++ restaurant_online_front/src/components/RestaurantForm.vue
@@ -1,5 +1,6 @@
 <script setup>
   import { ref } from 'vue';
+  import RegularFormStyle from './stylecomponents/RegularFormStyle.vue';
   const props = defineProps(['restaurant', 'actionName', 'label'])
   const emits = defineEmits(['formSubmit', 'cancel'])
 
@@ -14,95 +15,26 @@
 </script>
 
 <template>
-  <div class="card">
-    <h1>{{ label }}</h1>
-    <form id="rest_form">
-      <div class="card-content">
-        <div class="form-field">
-          <label for="r-name">Name: </label>
-          <input id='r-name' v-model="restaurant.name" v-on:change="inlcudeAttribute('name')" />
-        </div>
+  <RegularFormStyle>
+    <div class="form">
+      <div class="form__content">
+        <label for="r-name">Name</label>
+        <input id='r-name' v-model="restaurant.name" class="text-input" @change="inlcudeAttribute('name')" />
 
-        <div class="form-field">
-          <label for="r-email">Email: </label>
-          <input id='r-email' v-model="restaurant.email" v-on:change="inlcudeAttribute('email')" />
-        </div>
+        <label for="r-email">Email</label>
+        <input id='r-email' v-model="restaurant.email" class="text-input" @change="inlcudeAttribute('email')" />
         
-        <div class="form-field">
-          <label for="r-address">Address: </label>
-          <input id='r-address' v-model="restaurant.address" v-on:change="inlcudeAttribute('address')" />
-        </div>
+        <label for="r-address">Address</label>
+          <input id='r-address' v-model="restaurant.address" class="text-input" @change="inlcudeAttribute('address')" />
       </div>
 
-      <div class="card-actions">
+      <div class="form__actions">
         <button type="button" @click="$emit('formSubmit', modefiedRestaurant)">
           {{ actionName }}
         </button>
 
         <button type="button" @click="$emit('cancel')">back</button>
       </div>
-    </form>
-  </div>
+    </div>
+  </RegularFormStyle>
 </template>
-
-<style scoped>
-.card {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 400px;
-  height: 300px;
-  background-color: #fff;
-  border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0,0,0,.1);
-}
-
-.card-content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 80%;
-  height: 70%;
-}
-
-.form-field {
-  display: flex;
-  align-items: center;
-  margin-bottom: 16px;
-}
-
-label {
-  margin-right: 8px;
-}
-
-input {
-  padding: 8px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  width: 100%;
-}
-
-.card-actions {
-  display: flex;
-  justify-content: space-around;
-  width: 80%;
-  height: 30%;
-  margin-top: 16px;
-}
-
-button {
-  padding: 8px 16px;
-  border: none;
-  border-radius: 4px;
-  background-color: #007aff;
-  color: #fff;
-  cursor: pointer;
-  transition: all 0.3s ease;
-}
-
-button:hover {
-  background-color: #0059b3;
-}
-</style>
