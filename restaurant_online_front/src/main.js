d8e91efd6e99f66fea679fc23b197b64a657f6b4
anton <tehanovanton@gmail.com>
2023-05-08 10:31:28 +0300

    install bootstrap 5

--- restaurant_online_front/src/main.js
+++ restaurant_online_front/src/main.js
@@ -1,9 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css"
 import { createApp } from 'vue'
 
 import App from './App.vue'
 
 
 const app = createApp(App)
+import "bootstrap/dist/js/bootstrap.js"
 
 // pinia
 import { createPinia } from 'pinia'
